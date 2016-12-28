1c61bffabf4c8f5d3a3866ffb28db79afadcbd44
Alisa <pigulik@gmail.com>
2016-12-28 22:36:37 +0300

    login form, test event handling

--- frontend/src/app/components/loginForm.jsx
+++ frontend/src/app/components/loginForm.jsx
@@ -4,6 +4,26 @@ import React from "react";
 export class LoginForm extends React.Component {
     constructor(props,state){
         super(props);
+        this.state = {value: ''};
+        this.handleLoginChange = this.handleLoginChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleLoginChange(event) {
+        console.log(event);
+        this.setState({value: event.target.value});
+        console.log(this.props.value);
+    }
+
+    handlePasswordChange(event) {
+        this.setState({password: event.target.password});
+        console.log(this.props.password);
+    }
+
+    handleSubmit(event) {
+        alert('A login name and a password  were submitted: ' + this.state.value + ', ' + this.state.password);
+        event.preventDefault();
     }
 
     render() {
@@ -12,9 +32,15 @@ export class LoginForm extends React.Component {
         return (
             <div id="wrapper" className={this.props.show ? 'visible' : ''}>
 
-                <div className="login-form">
-                    <input type="text"/>
-                    <input type="password"/>
+                <div className="login-form" onSubmit={this.handleSubmit}>
+                    <label>
+                        Пользовательское имя:
+                        <input type="text" value={this.state.value} onChange={this.handleLoginChange} />
+                    </label>
+                    <label>
+                        Пароль:
+                        <input type="password" value={this.state.password} onChange={this.handlePasswordChange}/>
+                    </label>
                     <input type="submit" value={'Login'}/>
                 </div>
             </div>
